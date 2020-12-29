a5801cdeb2fb496266c82c3c524a4d216ded69d2
jinhong-park <jinhong.park.jhp@gmail.com>
2020-12-29 18:01:19 +0900

    “update”

--- _site/js/main.js
+++
@@ -1,9 +0,0 @@
-var checkbox = document.getElementById('themer');
-var invertor = document.getElementById('inverter');
-
-checkbox.addEventListener('change', function () {
-  // Triggers repaint in most browsers:
-  invertor.setAttribute('media', this.checked ? 'screen' : 'none');
-  // Forces repaint in Chrome:
-  invertor.textContent = invertor.textContent.trim();
-});
